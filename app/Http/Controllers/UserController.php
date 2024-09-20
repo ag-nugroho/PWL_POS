5a6b49bd2402a04f5883697915d8acaec9e4f6e9
ag-nugroho <nugrohoagung757@gmail.com>
2024-09-20 08:24:17 +0700

    Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Psy\Readline\Userland;
 
 class UserController extends Controller
 {
@@ -31,15 +32,24 @@ class UserController extends Controller
         // ];
         // UserModel::create($data);
 
-        $data = [
-            'level_id' => 3,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 3,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::create($data);
         
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // $user = UserModel::find(1);
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 1);
+        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
