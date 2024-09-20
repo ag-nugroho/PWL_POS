51530a3a89e5110d831bf99079c02c0e9ef9787e
ag-nugroho <nugrohoagung757@gmail.com>
2024-09-20 09:08:58 +0700

    Praktikum 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -52,8 +52,40 @@ class UserController extends Controller
 
         // $user = UserModel::findOrFail(1);
         // $user = UserModel::where('username', 'manager9')->firstOrFail();
-        $user = UserModel::where('level_id', 2)->count();
+        // $user = UserModel::where('level_id', 2)->count();
         // dd(user);
+
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ],
+        // );
+
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'level_id' => 2,
+        //         'password' => Hash::make('12345'),
+        //     ],
+        // );
+
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ],
+        // );
+
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'level_id' => 2,
+                'password' => Hash::make('12345'),
+            ],
+        );
         return view('user', ['data' => $user]);
     }
 }
